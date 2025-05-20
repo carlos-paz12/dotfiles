3c5596de9cbc2615b3980320accfff449e40dfeb
carlos-paz12 <capz.devel@gmail.com>
2025-05-19 21:53:47 -0300

    add variables files to zsh

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,7 +1,28 @@
-alias "mvv"="mv --verbose"
-alias "cpv"="cp --verbose"
-alias "ls"="eza --icons --no-quotes --sort type"
+# ~/.dotfiles/zsh/.oh-my-zsh/custom/aliases.zsh
+# ─────────────────────────────────────────────────────────────
 
-alias "tree"="ls --tree"
-alias "sudo apt"="sudo nala"
-alias "top"="btm"
+# Navegação por diretórios
+alias dot='cd $DOTFILES'
+alias p1='cd $P1'
+alias edb2='cd $P2'
+alias tcp='cd $TCC'
+alias cl='cd $LAB'
+alias ufrn='cd $UFRN'
+
+# Comandos do sistema / produtividade
+alias c='clear'
+alias top='btm'
+
+# Gerenciador de pacotes (nala)
+alias install='sudo nala install -y'
+alias uninstall='sudo nala remove -y'
+alias update='sudo nala update'
+alias upgrade='sudo nala upgrade'
+
+# Manipulação de arquivos
+alias mvv='mv --verbose'
+alias cpv='cp --verbose'
+
+# Visualização com eza (ls moderno)
+alias ls='eza --icons --no-quotes --sort type'
+alias tree='la --tree'
---
+++ zsh/.oh-my-zsh/custom/variables.zsh
@@ -0,0 +1,9 @@
+# ~/.dotfiles/zsh/.oh-my-zsh/custom/variables.zsh
+# ─────────────────────────────────────────────────────────────
+
+export DOTFILES=~/.dotfiles
+export UFRN=~/Documents/UFRN
+export P1=$UFRN/P1
+export EDB2=$UFRN/EDB2
+export TCP=$UFRN/TCP
+export CL=$UFRN/CL
